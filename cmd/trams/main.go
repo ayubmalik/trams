435b02335cb63829c9e6ff0fd78360d3f9282ad2
Ayub Malik <ayub.malik@moneysupermarket.com>
2021-06-28 14:52:18 +0100

    feat: add empty sub commands
    Added empty sub commands and help text.

--- cmd/trams/main.go
+++ cmd/trams/main.go
@@ -10,10 +10,26 @@ import (
 
 func main() {
 	app := &cli.App{
-		Usage: "display Metrolink tram information using data from TfGM API",
-		Action: func(c *cli.Context) error {
-			fmt.Println("display stations")
-			return nil
+		Usage:     "display Metrolink tram information using data from TfGM API",
+		UsageText: "trams COMMAND [args]",
+		Commands: []*cli.Command{
+			{
+				Name:      "display",
+				Usage:     "display tram information for specified Metrolink stations. If no stations are specified displays all stations. Run `trams help display` for more details.",
+				UsageText: "display [STATION...] - If no STATION arguments are specified, displays all stations. Multiple STATION arguments can be specified as short name or long name, e.g. `display BCH MAN` or `display Benchill MAN`",
+				Action: func(c *cli.Context) error {
+					fmt.Println("TODO display: ", c.Args().First())
+					return nil
+				},
+			},
+			{
+				Name:  "clear",
+				Usage: "clear cached list of stations and temporary data",
+				Action: func(c *cli.Context) error {
+					fmt.Println("TODO clear: ", c.Args().First())
+					return nil
+				},
+			},
 		},
 	}
 
